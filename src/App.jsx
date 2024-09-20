c1a969edfc2130e0fd0990f3c19b0d5551398334
VictoriaCk55 <noemi.victoria.ck.vargas@gmail.com>
2024-09-20 14:47:03 -0400

    This is lab 4 of react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { WapperList } from './components/WrapperList'
+
+function App() {
+  return (
+    <>
+      <WapperList>
+        <ul>
+          <li> Pan </li>
+          <li> Queso </li>
+          <li> Mantequilla </li>
+        </ul>  
+      </WapperList>
+    </>
+  )
+}
+
+export default App
